17bf02770c9b71c697847dba6dfe187ea23234cd
zeffmubashir <zeffmubashir@gmail.com>
2026-06-28 11:58:18 +0500

    Add missing tailwind plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -10,6 +11,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
